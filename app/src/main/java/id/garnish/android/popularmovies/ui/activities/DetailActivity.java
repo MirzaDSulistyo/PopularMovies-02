d3996ce9d08a19fe57d66536bbe0d1513afe4e1d
mirzadhanu <mirzadanu@ymail.com>
2017-09-09 13:38:01 +0700

    clean DetailActivity

--- app/src/main/java/id/garnish/android/popularmovies/ui/activities/DetailActivity.java
+++ app/src/main/java/id/garnish/android/popularmovies/ui/activities/DetailActivity.java
@@ -1,19 +1,14 @@
 package id.garnish.android.popularmovies.ui.activities;
 
 import android.content.Intent;
-import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.bumptech.glide.Glide;
 import com.squareup.picasso.Picasso;
-
-import java.text.ParseException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -21,7 +16,6 @@ import id.garnish.android.popularmovies.BuildConfig;
 import id.garnish.android.popularmovies.R;
 import id.garnish.android.popularmovies.models.Movie;
 import id.garnish.android.popularmovies.ui.fragments.DetailFragment;
-import id.garnish.android.popularmovies.utilities.DateTimeHelper;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -65,37 +59,6 @@ public class DetailActivity extends AppCompatActivity {
                     .add(R.id.movie_detail_container, fragment)
                     .commit();
         }
-
-//        tvOriginalTitle.setText(movie.getOriginalTitle());
-//
-//        Picasso.with(this)
-//                .load(movie.getPosterPath())
-//                .error(R.drawable.not_found)
-//                .placeholder(R.drawable.searching)
-//                .into(ivPoster);
-//
-//        String overview = movie.getOverview();
-//        if (overview == null) {
-//            tvOverView.setTypeface(null, Typeface.ITALIC);
-//            overview = getString(R.string.no_summary_found);
-//        }
-//
-//        tvOverView.setText(overview);
-//        tvVoteAverage.setText(movie.getDetailedVoteAverage());
-//
-//        String releaseDate = movie.getReleaseDate();
-//        if (releaseDate != null) {
-//            try {
-//                releaseDate = DateTimeHelper.getLocalizedDate(this,
-//                        releaseDate, movie.getDateFormat());
-//            } catch (ParseException e) {
-//                e.printStackTrace();
-//            }
-//        } else {
-//            tvReleaseDate.setTypeface(null, Typeface.ITALIC);
-//            releaseDate = getString(R.string.no_release_date_found);
-//        }
-//        tvReleaseDate.setText(releaseDate);
     }
 
     @Override
